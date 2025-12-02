f7e79257c8d116b2163d2ad6a91378a3183e687e
Telem Tobi <telem.tobi@recapp.co>
2025-12-02 11:13:15 +0200

    Comment out `calculateBufferWindows`

--- Sources/PlayKit/UIPlaylistView.swift
+++ Sources/PlayKit/UIPlaylistView.swift
@@ -44,7 +44,7 @@ public final class UIPlaylistView: UIView {
             subscribeToCurrentIndex()
             subscribeToRate()
             prepareCurrentPlayer()
-            calculateBufferWindows()
+//            calculateBufferWindows()
         }
     }
     

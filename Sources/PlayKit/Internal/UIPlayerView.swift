8a0b55f773286398c22a82c66985188fc7628783
Telem Tobi <telem.tobi@recapp.co>
2025-12-05 09:47:12 +0200

    Revert cancel method change

--- Sources/PlayKit/Internal/UIPlayerView.swift
+++ Sources/PlayKit/Internal/UIPlayerView.swift
@@ -148,7 +148,6 @@ final class UIPlayerView: UIView {
         player.cancelPendingPrerolls()
         player.replaceCurrentItem(with: nil)
         readyObserver = nil
-        status.value = .ready
         statusSubscription?.cancel()
         imageView.image = nil
         progressInSeconds.value = .zero

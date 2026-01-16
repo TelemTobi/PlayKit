5ee052aa0969a59337773940a68caa96d6c467a4
Telem Tobi <telem.tobi@recapp.co>
2026-01-16 08:40:29 +0200

    Add isPlaying to PlaylistController init

--- Sources/PlayKit/Playlist/PlaylistController.swift
+++ Sources/PlayKit/Playlist/PlaylistController.swift
@@ -96,11 +96,13 @@ public final class PlaylistController: ObservableObject, Identifiable {
         initialIndex: Int = .zero,
         backwardBuffer: Int = 2,
         forwardBuffer: Int = 5,
-        isFocused: Bool = false
+        isFocused: Bool = false,
+        isPlaying: Bool = false
     ) {
         self.id = id
         self.items = items
         self.isFocused = isFocused
+        self.isPlaying = isPlaying
         self.backwardBuffer = backwardBuffer
         self.forwardBuffer = forwardBuffer
         
